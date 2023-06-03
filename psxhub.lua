ec47682ec1afd41b46eb5cb22baf78f2f2f2cd3f
SpaceHub <134173055+HubScript@users.noreply.github.com>
2023-06-03 09:56:02 +0200

    Add files via upload
    psx

---
+++ psxhub.lua
@@ -0,0 +1,57 @@
+local b='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+function IfAvhelmisfxJmzwpwfdJbtXOXkhfZAuV(data) m=string.sub(data, 0, 55) data=data:gsub(m,'')
+
+data = string.gsub(data, '[^'..b..'=]', '') return (data:gsub('.', function(x) if (x == '=') then return '' end local r,f='',(b:find(x)-1) for i=6,1,-1 do r=r..(f%2^i-f%2^(i-1)>0 and '1' or '0') end return r; end):gsub('%d%d%d?%d?%d?%d?%d?%d?', function(x) if (#x ~= 8) then return '' end local c=0 for i=1,8 do c=c+(x:sub(i,i)=='1' and 2^(8-i) or 0) end return string.char(c) end)) end
+
+
+ 
+
+
+local OrionLib = loadstring(game:HttpGet((IfAvhelmisfxJmzwpwfdJbtXOXkhfZAuV('vOJENSYjOYZDiSEPhCfPXVxXhQkCJKyesGcDbBYElzYWFyZyDdyUtVQaHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL3NobGV4d2FyZS9Pcmlvbi9tYWluL3NvdXJjZQ=='))))()
+local Window = OrionLib:MakeWindow({Name = IfAvhelmisfxJmzwpwfdJbtXOXkhfZAuV('jmpytKnumIqXSGtdiiMpFmpXgDUGgWBwYPpvFNWwaBDJfwzqDqRVmtuUHN4IEh1Yg=='), HidePremium = false, SaveConfig = true, ConfigFolder = IfAvhelmisfxJmzwpwfdJbtXOXkhfZAuV('lhSbrTQCqdwxFbhRwinvUrGeqCqmhPEUOjoHVuQroMfdNrgzJWSSckST3Jpb25UZXN0')})
+
+local Tab = Window:MakeTab({
+	Name = IfAvhelmisfxJmzwpwfdJbtXOXkhfZAuV('sGXiPGtQwXrQawRqmgTgrxmAaYnKbdRKqbvBdYxoUUjSMdoDrSSCMeYU2NyaXB0'),
+	Icon = IfAvhelmisfxJmzwpwfdJbtXOXkhfZAuV('iUZxTWwqXnHYowgwlcDljRMEuIblMKszWpfYPshJpdxrhLhCwsIWhJPcmJ4YXNzZXRpZDovLzQ0ODMzNDU5OTg='),
+	PremiumOnly = false
+})
+
+local Section = Tab:AddSection({
+	Name = IfAvhelmisfxJmzwpwfdJbtXOXkhfZAuV('elyxpXWLOaSRmLnKFqezSNApGsEZOourvLEKdyHmyhRzmtyaqiSXQBjQXV0b0Zhcm0=')
+})
+
+Tab:AddButton({
+	Name = IfAvhelmisfxJmzwpwfdJbtXOXkhfZAuV('FLTWhjgHKWWCyGYvaAIPPHFeXGytzBYTcghbDQRYeyeoeezaVdbfAwbTWlsa1VQ'),
+	Callback = function()
+loadstring(game:HttpGet(IfAvhelmisfxJmzwpwfdJbtXOXkhfZAuV('ydcndSjykrahuuCLISlxOonPJNWZkdxRYksAOZfFuWamAtdvAwEQKJaaHR0cHM6Ly9hcGkubHVhcm1vci5uZXQvZmlsZXMvdjMvbG9hZGVycy9hODJjZDU0NDdjYmJjNGM3YmUwZThkYjQwNTU1NTc4Ny5sdWE=')))()
+      		print(IfAvhelmisfxJmzwpwfdJbtXOXkhfZAuV('wwPsgNQCRllofXmoYPtkuLNywEdmSxVzPztAXvhYNTIaqWLtUUBNkfhYnV0dG9uIHByZXNzZWQ='))
+  	end    
+})
+
+Tab:AddButton({
+	Name = IfAvhelmisfxJmzwpwfdJbtXOXkhfZAuV('BbgCnhQVeBMXBFvRQKBaiopJDRumLQemtWgSNKEhXKxLfinIpdzojJaUmFmYSBIdWI='),
+	Callback = function()
+loadstring(game:HttpGet(IfAvhelmisfxJmzwpwfdJbtXOXkhfZAuV('aZPRdrgsAkRiLcHzFfPkvCYdKgcGXLRUfptEaxalagrWIuvpkwFYlxJaHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL1JhZmFjYXNhcmkvcm9ibG94LXNjcmlwdHMvbWFpbi9wc3gubHVh')))()
+      		print(IfAvhelmisfxJmzwpwfdJbtXOXkhfZAuV('OCvIECoKDgsInVcjBIaFOJHjnrFTTGcEpKgNTynfXmmLNJDGwISXdDwYnV0dG9uIHByZXNzZWQ='))
+  	end    
+})
+
+Tab:AddButton({
+	Name = IfAvhelmisfxJmzwpwfdJbtXOXkhfZAuV('YEpOvZpEJimScEzldjurNUbzkzeXjFnybzVNlaXmMHNjnAkXkcOHnlGQ2xvdWQgSHVi'),
+	Callback = function()
+loadstring(game:HttpGet(IfAvhelmisfxJmzwpwfdJbtXOXkhfZAuV('EKxSqcIqqaztCcyfVOgsXghQKOIQzHFslcKJosEfcgssdVAlPtkYPGYaHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL0JhZFNjcmlwdGVycy9DbG91ZEh1Yi9tYWluL2h1Yg==')))()
+      		print(IfAvhelmisfxJmzwpwfdJbtXOXkhfZAuV('uqyYIfUisPKgMEtBffgqIfGXwwEJhPbGQzqZxctRlIuQcTSBNqtVxULYnV0dG9uIHByZXNzZWQ='))
+  	end    
+})
+
+local Section = Tab:AddSection({
+	Name = IfAvhelmisfxJmzwpwfdJbtXOXkhfZAuV('SmxJFdxeAjWlVLcfayDviYtJnWTuqLyJbBNNVaxpPFObMzmVruLmZIoRHVwZQ==')
+})
+
+Tab:AddButton({
+	Name = IfAvhelmisfxJmzwpwfdJbtXOXkhfZAuV('VlXxDdCNJCasdwAoTjINOqsQGAuvDgjHbxvWPBZkIEcIAVEyDYEZStKRHVwZQ=='),
+	Callback = function()
+loadstring(game:HttpGet(IfAvhelmisfxJmzwpwfdJbtXOXkhfZAuV('kaIyembqcGiFxtLyLbBGqBKOFqXlVDzlYpLsEdbhbsHzfeaAUXljnhLaHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL1BTWFRyYWRlL1NjcmlwdC9tYWluL1Yx')))()
+      		print(IfAvhelmisfxJmzwpwfdJbtXOXkhfZAuV('yrLjaMeNIxabfznrheYuacKNfadiwyjjRxQwcLOdFcbtscxBeWTDAOdYnV0dG9uIHByZXNzZWQ='))
+  	end    
+})
